0c900a1c32fe13681961c3815145d4252b8b8f74
ervcabrita <45874217+ervcabrita@users.noreply.github.com>
2019-01-07 18:52:16 +0000

    Add files via upload
    draft_update

---
+++ 1-Automation/TS.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0b8e6b9e-572c-4b12-b6b0-b0e68e8fd87f</testSuiteGuid>
+   <testCaseLink>
+      <guid>f96ee982-e77d-4728-ac73-804e2bad35b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/automation_script_annotation</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
